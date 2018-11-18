bb4e5b7e19207c444c6064941bb39b8ce3ae1a95
ludeeus <joasoe@gmail.com>
2018-11-18 19:37:16 +0100

    Added get_device_type to Bluetooth class

--- ghlocalapi/bluetooth.py
+++ ghlocalapi/bluetooth.py
@@ -94,3 +94,18 @@ class Bluetooth(object):
     def devices(self):
         """Return the device info if any."""
         return self._devices
+
+
+def get_device_type(device_type=0):
+    """Return the device type from a device_type list."""
+    device_types = {
+        0: "Unknown",
+        1: "Classic - BR/EDR devices",
+        2: "Low Energy - LE-only",
+        3: "Dual Mode - BR/EDR/LE"
+    }
+    if device_type in [0, 1, 2, 3]:
+        return_value = device_types[device_type]
+    else:
+        return_value = device_types[0]
+    return return_value
