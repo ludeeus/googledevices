41bb3a4506cf33db51e066ee2b57813b64bc36ce
ludeeus <joasoe@gmail.com>
2018-11-21 22:45:39 +0100

    Added scan_for_devices_multi_run

--- ghlocalapi/bluetooth.py
+++ ghlocalapi/bluetooth.py
@@ -85,6 +85,42 @@ class Bluetooth(object):
                 aiohttp.ClientError, socket.gaierror) as error:
             _LOGGER.error('Error connecting to GHLocalApi, %s', error)
 
+    async def scan_for_devices_multi_run(self, runs=2):
+        """Scan for devices multiple times."""
+        run = 1
+        master = {}
+        while run < runs + 1:
+            await self.scan_for_devices()
+            await self.get_scan_result()
+            if master is None:
+                for device in self._devices:
+                    mac = device['mac_address']
+                    master[mac] = {}
+                    master[mac]['rssi'] = device['rssi']
+                    master[mac]['device_class'] = device['device_class']
+                    master[mac]['name'] = device['name']
+                    master[mac]['device_type'] = device['device_type']
+                    master[mac]['count'] = 1
+            else:
+                for device in self._devices:
+                    mac = device['mac_address']
+                    if master.get(mac, False):
+                        master[mac]['rssi'] = device['rssi']
+                        master[mac]['count'] = str(1 + 1)
+                    else:
+                        master[mac] = {}
+                        master[mac]['rssi'] = device['rssi']
+                        master[mac]['device_class'] = device['device_class']
+                        master[mac]['name'] = device['name']
+                        master[mac]['device_type'] = device['device_type']
+                        master[mac]['count'] = 1
+            run = run + 1
+            result = []
+            for device in master:
+                if int(master[device]['count']) > 1:
+                    result.append(master[device])
+        self._devices = result
+
     @property
     def status(self):
         """Return the the bluetooth status of the device."""
