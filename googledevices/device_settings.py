ee398e0cee82b23e536d1faf076cf53704892986
Joakim Sørensen <joasoe@gmail.com>
2018-11-23 16:38:57 +0100

    Added set_eureka_info and enable_control_notifications (#2)
    * Added set_eureka_info and enable_control_notifications
    
    * rename to control_notifications

--- googledevices/device_settings.py
+++ googledevices/device_settings.py
@@ -41,3 +41,37 @@ class DeviceSettings(object):
                 aiohttp.ClientError, socket.gaierror) as error:
             _LOGGER.error('Error connecting to googledevices, %s', error)
         return returnvalue
+
+    async def set_eureka_info(self, data):
+        """Set eureka info."""
+        endpoint = '/setup/set_eureka_info'
+        url = API.format(ip=self._ipaddress, endpoint=endpoint)
+        returnvalue = False
+        try:
+            async with async_timeout.timeout(5, loop=self._loop):
+                result = await self._session.post(url, json=data,
+                                                  headers=HEADERS)
+                if result.status == 200:
+                    returnvalue = True
+        except (asyncio.TimeoutError,
+                aiohttp.ClientError, socket.gaierror) as error:
+            _LOGGER.error('Error connecting to googledevices, %s', error)
+        return returnvalue
+
+    async def control_notifications(self, active):
+        """Set control_notifications option."""
+        endpoint = '/setup/set_eureka_info'
+        url = API.format(ip=self._ipaddress, endpoint=endpoint)
+        value = 1 if active else 2
+        data = {'settings': {'control_notifications': value}}
+        returnvalue = False
+        try:
+            async with async_timeout.timeout(5, loop=self._loop):
+                result = await self._session.post(url, json=data,
+                                                  headers=HEADERS)
+                if result.status == 200:
+                    returnvalue = True
+        except (asyncio.TimeoutError,
+                aiohttp.ClientError, socket.gaierror) as error:
+            _LOGGER.error('Error connecting to googledevices, %s', error)
+        return returnvalue
