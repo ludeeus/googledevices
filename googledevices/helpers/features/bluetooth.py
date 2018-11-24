f7b2fb549c9b623abf28856d2bd7722775b53b02
Eliseo Martelli <me@eliseomartelli.it>
2018-11-24 19:13:47 +0100

    moved features inside helpers

--- googledevices/helpers/features/bluetooth.py
+++ googledevices/helpers/features/bluetooth.py
@@ -1,6 +1,7 @@
 from googledevices.utils.features import Feature
 
 class Bluetooth(Feature):
+    """Representation of Bluetooth feature."""
 
     def has_feature(self, device_info):
         """Return if it has the feature."""
