bdfe0fe868e925c06acef86fe835f74b6406742a
Joakim Sørensen <joasoe@gmail.com>
2018-11-30 10:57:45 +0100

    Adds new scan to CLI (#17)
    * Adds new scan to CLI
    
    * option
    
    * wrong key
    
    * dump json
    
    * format

--- googledevices/cli/commands.py
+++ googledevices/cli/commands.py
@@ -27,6 +27,51 @@ def device_info(ip_address):
 
 
 @commands.command()
+@click.option('--subnet', type=str, default=None, help="Format 0.0.0.0/00")
+def get_all_devices(subnet):
+    """Get information about all devices on your network."""
+    from googledevices.api.bluetooth import Bluetooth
+    from googledevices.utils.scan import NetworkScan
+    from googledevices.api.device_info import DeviceInfo
+
+    async def bluetooth_scan():
+        """Get devices from all GH units on the network."""
+        if not subnet:
+            import netifaces
+            gateway = netifaces.gateways().get('default', {})
+            ipscope = gateway.get(netifaces.AF_INET, ())[0][:-1] + '0/24'
+        else:
+            ipscope = subnet
+        devices = {}
+        async with aiohttp.ClientSession() as session:
+            googledevices = NetworkScan(LOOP, session)
+            result = await googledevices.scan_for_units(ipscope)
+        for host in result:
+            if host['assistant']:
+                async with aiohttp.ClientSession() as session:
+                    googledevices = DeviceInfo(LOOP, session, host['host'])
+                    await googledevices.get_device_info()
+                    ghname = googledevices.device_info.get('name')
+                async with aiohttp.ClientSession() as session:
+                    googledevices = Bluetooth(LOOP, session, host['host'])
+                    await googledevices.scan_for_devices_multi_run()
+                    await googledevices.get_scan_result()
+                    for device in googledevices.devices:
+                        mac = device['mac_address']
+                        if not devices.get(mac, False):
+                            # New device
+                            devices[mac] = {}
+                            devices[mac]['rssi'] = device['rssi']
+                            devices[mac]['ghunit'] = ghname
+                        elif devices[mac]['rssi'] < device['rssi']:
+                            # Better RSSI value on this device
+                            devices[mac]['rssi'] = device['rssi']
+                            devices[mac]['ghunit'] = ghname
+        print(json.dumps(devices, indent=4, sort_keys=True))
+    LOOP.run_until_complete(bluetooth_scan())
+
+
+@commands.command()
 @click.option('--network', '-N', type=str, default=None,
               help="The network you want to scan\
               in this format '192.168.1.0/24'.")
